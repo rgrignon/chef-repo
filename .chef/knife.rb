920872e8a0abd3a4c0a74ef55e55d7ccf2d73c7a
Robert Grignon <robert@unifiedhosting.com>
2014-01-09 13:26:41 -0500

    Initial Commit

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'rgrignon'
+client_key               'C:/Users/rgrignon/.chef/rgrignon.pem'
+validation_client_name   'chef-validator'
+validation_key           'C:/Users/rgrignon/chef-validator.pem'
+chef_server_url          'https://cent-chef-server'
+syntax_check_cache_path  'C:/Users/rgrignon/.chef/syntax_check_cache'
