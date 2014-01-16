cc59489622b3b6c6b074da40fdf8ae6d019aba51
Robert Grignon <robert@unifiedhosting.com>
2014-01-16 00:21:19 -0500

    Added a few more items

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -10,6 +10,16 @@
 # chef-repo\cookbooks\apache\recipes\default.rb
 
 # install apache
+package_name = "apache2"
+service_name = "apache2"
+document_root = "/var/www"
+
+if node["platform"] == "centos"
+package_name = "httpd"
+service_name = "httpd"
+document_root = "/var/www/html"
+end	
+
 package "apache2" do
   action :install
 end
@@ -21,7 +31,14 @@ service "apache2" do
 end
 
 #write our home page 
-cookbook_file "/var/www/index.html" do
-  source "index.html"
-  mode "0644"
+# cookbook_file "#{document_root}/index.html" do
+#   source "index.html"
+#   mode "0644"
+# end
+
+template "#{document_root}/index.html" do
+	source "index.html.erb"
+	owner "root"
+	group "root"
+	mode "0644"
 end
